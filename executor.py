869decab57af10166aae5ca50eb1dea029f5804d
DarriusL <darrius.lei@outlook.com>
2023-05-17 10:00:58 +0800

    [2023.05.17]Roughly completed simulation system.

--- executor.py
+++ executor.py
@@ -1,9 +1,10 @@
 # @Time   : 2023.05.15
 # @Author : Darrius Lei
 # @Email  : darrius.lei@outlook.com
-import os, logging
+import os, logging, argparse
 from lib import glb_var
 from lib.callback import Logger
+from room import run_work
 
 if __name__ == '__main__':
     if not os.path.exists("./cache/logger"):
@@ -14,3 +15,12 @@ if __name__ == '__main__':
         filename = './cache/logger/logger.log',
     ).get_log()
     glb_var.set_value('logger', log);
+    #set arg
+    parse = argparse.ArgumentParser();
+    parse.add_argument('--config', '-cfg', type = str, default = None, help = 'Path of configration.');
+    parse.add_argument('--mode', type = str, default = 'train', help = 'Mode of operation.');
+
+    args = parse.parse_args();
+
+    if args.config is not None:
+        run_work(args.config, args.mode);
--- room/work.py
+++ room/work.py
@@ -2,7 +2,28 @@
 # @Author : Darrius Lei
 # @Email  : darrius.lei@outlook.com
 from lib import json_util, util, glb_var
+from room.system import System
+import torch, time
 
-def run_work():
+def run_work(cfg_path, mode):
     lab_cfg = json_util('./config/lab_cfg.json');
-    util.set_attr(glb_var, lab_cfg['constant']);
+    glb_var.set_values(lab_cfg['constant'])
+    #set device
+    if lab_cfg['general']:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu");
+    else:
+        device = torch.device("cpu");
+    glb_var.set_value('device', device);
+    #load config
+    cfg = json_util.jsonload(cfg_path);
+    #generate system
+    system = System(cfg);
+
+    if mode == 'train':
+        run_train(system);
+
+def run_train(system):
+    t = time.time();
+    glb_var.get_value('logger').info('Start training ... ');
+    system.train();
+    glb_var.get_value('logger').info(f'Training complete, time consuming: {util.s2hms(time.time() - t)}');
